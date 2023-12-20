fb4d7f8efe9befcf24d8fc48539200f7a8658b29
JuniorProgrammR <154478078+JuniorProgrammR@users.noreply.github.com>
2023-12-20 16:53:12 -0700

    Create Board.java
    The specification for the Board class is as follows:
    
    Variables
    private String[][] squares - An array which represents the board on which a player places their battleship and records shots.
    
    Constructors
    public Board() - This constructor initializes the squares array with every value set to "-", which is the String used to represent a blank square.
    
    Methods
    public String toString() - Returns a multi-line representation of the board by concatenating all the values in squares with a new line for each row and spaces separating the Strings in each row.
    public boolean addShip(int row, int col, int len, boolean horizontal) - Attempts to add a ship of length len to the grid, starting at the row and column specified and proceeding either rightwards (if horizontal is true), or downwards (if horizontal is false). If the ship can be placed in the place specified, each square making up the ship should be set to "b", and the method should return true. A ship may not be placed if it would go off the grid, or would intersect another ship on the grid. If the ship cannot be placed, no values in squares should be changed and the method should return false.
    public boolean foundShip(int len) - Search the board for any possible ships of length len. If there are exactly len consecutive squares (either horizontal or vertical) containing a "b" String somewhere in the grid, then return true, otherwise, return false.
    public int shoot(int row, int col) - If row and col specify a square which is out of bounds, the method should return -1. If the square at the specified row and column contains "-" (i.e. is blank), the square should be changed to "m" to signify a miss, and the method should return 0. If the square contains "b" (i.e. a battleship which hasn't been hit yet) this square should be changed to an "x" to signify a hit, and the method should return 1. If the square contains either "x" or "m" the method should return 2 (these are squares which have already been “shot”).
    public boolean gameOver() - Returns true only when the board contains no un-sunken ships. Otherwise, return false.
    Along with your Board class there is another class in a separate file named Battleship. This class is a simple implementation of a game which allows you to add ships to a single Battleship board using user input and shoot at these ships. A sample run of the main method from this class is shown below.

---
+++ Board.java
@@ -0,0 +1,191 @@
+//Assignment 8
+import java.util.ArrayList;
+public class Board{
+
+  private String[][] squares;
+  private ArrayList<Ship> ships=new ArrayList<Ship>();
+  private static int floating=0;
+
+  public Board() //Default Constructor
+  {
+    this(10,10);
+  }
+  public Board(int y,int x)
+  {
+    squares=new String[y][x];
+    for(y=0;y<squares.length;y++)//Row Major
+    {
+      for(x=0;x<squares[y].length;x++)//Column Minor
+      {
+        squares[y][x]="-";
+      }
+    }
+  }
+
+  public String toString()
+  {
+    String output="";
+    for(int y=0;y<squares.length;y++)//Row Major
+    {
+      for(int x=0;x<squares[y].length;x++)//Column Minor
+      {
+        output+=squares[y][x]+" ";
+      }
+      output=output.substring(0,output.length()-1)+"\n";
+    }
+    return output;
+  }
+
+  public boolean addShip(int row, int col, int len, boolean horizontal){
+    if(horizontal)
+    {
+      if(len+col<=squares[0].length&&row<squares.length&&row>=0&&col>=0)
+      {
+        boolean placeable=true;
+        int rightRie=col+len;
+        for(int index=col;index<rightRie;index++)
+        {
+          if(squares[row][index].equals("b"))
+          {
+            placeable=false;
+            break;
+          }
+        }
+        if(placeable)
+        {
+          for(int index=col;index<rightRie;index++)
+          {
+            squares[row][index]="b";
+          }
+          ships.add(new Ship(len, row, col));
+          floating+=len;
+          return true;
+        }
+        else
+        {
+          return false;
+        }
+      }
+      else
+      {
+        return false;
+      }
+    }
+    else //if vertical
+    {
+      if(len+row<=squares.length&&col<squares[0].length&&col>=0&&row>=0)
+      {
+        boolean placeable=true;
+        int bottomRie=row+len;
+        for(int index=row;index<bottomRie;index++)
+        {
+          if(squares[index][col].equals("b"))
+          {
+            placeable=false;
+            break;
+          }
+        }
+        if(placeable)
+        {
+          for(int index=row;index<bottomRie;index++)
+          {
+            squares[index][col]="b";
+          }
+          ships.add(new Ship(len, row, col));
+          floating+=len;
+          return true;
+        }
+        else
+        {
+          return false;
+        }
+      }
+      else
+      {
+        return false;
+      }
+    }
+  }
+
+  public boolean foundShip(int len){
+    for(int index=0;index<ships.size();index++)
+    {
+      if(ships.get(index).getLen()==len)
+      {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public int shoot(int row, int col){
+    if(row<0||col<0||row>=squares.length||col>=squares[0].length)
+    {
+      return -1;
+    }
+    else if(squares[row][col].equals("-"))
+    {
+      squares[row][col]="m";
+      return 0;
+    }
+    else if(squares[row][col].equals("b"))
+    {
+      squares[row][col]="x";
+      floating--;
+      return 1;
+    }
+    else //(squares[row][col].equals("m")||squares[row][col].equals("x"))
+    {
+      return 2;
+    }
+  }
+
+  public boolean gameOver(){
+    if(floating==0)
+    {
+      return true;
+    }
+    return false;
+    /*boolean done=true;
+    for(int index=0;index<squares.length;index++)
+    {
+      for(int index2=0;index2<squares[index].length;index2++)
+      {
+        if(squares[index][index2].equals("b"))
+        {
+          done=false;
+          break;
+        }
+      }
+    if(!done)
+    {
+      break;
+    }
+    }
+  return done;*/
+  }
+private class Ship
+{
+  private int len;
+  private int row;
+  private int col;
+  public Ship(int len, int row, int col)
+  {
+    this.len=len;
+    this.row=row;
+    this.col=col;
+  }
+  public int getLen()
+  {
+    return len;
+  }
+  public int getRow()
+  {
+    return row;
+  }
+  public int getCol()
+  {
+    return col;
+  }
+}
+}
