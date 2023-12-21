e72b24499e16ce1944c737355165ed84c230ee83
JuniorProgrammR <154478078+JuniorProgrammR@users.noreply.github.com>
2023-12-20 17:03:54 -0700

    Create Assignment9_Coach.java

---
+++ Assignment9_Coach.java
@@ -0,0 +1,15 @@
+public class Assignment9_Coach extends Person
+{
+  private String role;
+  
+  public Coach(String firstName, String lastName, String role)
+  {
+    super(firstName,lastName);
+    this.role=role;
+  }
+  
+  public String toString()
+  {
+    return super.toString()+"\n   Role: "+role;
+  }
+}
